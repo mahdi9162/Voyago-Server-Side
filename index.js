2bd8e521ea26c1f0cf073a4d6b3fd9edcd810522
Mahdi Hasan <hasanmahdi6000@gmail.com>
2025-11-10 21:25:46 +0600

    Add DELETE /bookings/:id route using deleteOne()

--- index.js
+++ index.js
@@ -106,6 +106,14 @@ async function run() {
       res.send(result);
     });
 
+    // DELETE API
+    app.delete('/bookings/:id', async (req, res) => {
+      const { id } = req.params;
+      const objectId = new ObjectId(id);
+      const result = await bookingsCollection.deleteOne({ _id: objectId });
+      res.send(result);
+    });
+
     await client.db('admin').command({ ping: 1 });
     console.log('Pinrd your deployment. You successfully connected to MongoDB!');
   } finally {
